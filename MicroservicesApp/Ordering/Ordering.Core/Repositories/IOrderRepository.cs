861a8da0a91e4cb707ed088f879f3a7dfa22a356
safayatzisan <zisan_cse_mbstu@hotmail.com>
2021-02-27 20:52:56 +0600

    add generic repositories

---
+++ MicroservicesApp/Ordering/Ordering.Core/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories.Base;
+
+namespace Ordering.Core.Repositories
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        // As this is custom business, we are putting this here instead of IRepository.
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
