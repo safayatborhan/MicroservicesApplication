366dfbe12029a6658348634be896b1a9489dedb9
safayatzisan <zisan_cse_mbstu@hotmail.com>
2021-03-27 20:52:46 +0600

    implement basic api of order

--- MicroservicesApp/Ordering/Ordering.API/Controllers/OrderController.cs
+++ MicroservicesApp/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -1,14 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Commands;
+using Ordering.Application.Queries;
+using Ordering.Application.Responses;
 
 namespace Ordering.API.Controllers
 {
+    /*
+     * Required packages:
+     * 1. Automapper
+     * 2. MediatR
+     * 3. MediatR.Extensions.Microsoft.DependencyInjection
+     * 4. Swashbuckle.AspNetCore
+     */
     [ApiController]
     [Route("[controller]")]
     public class OrderController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<OrderResponse>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrderResponse>>> GetOrdersByUserName(string userName)
+        {
+            var query = new GetOrderByUserNameQuery(userName);
+
+            var orders = await _mediator.Send(query);
+
+            return Ok(orders);
+        }
+
+        /*
+         * This api is for testing purpose.
+         * This will mainly be consumed from Basket microservice.
+         */
+        [HttpPost]
+        [ProducesResponseType(typeof(OrderResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<OrderResponse>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            return Ok(result);
+        }
     }
 }
--- MicroservicesApp/Ordering/Ordering.API/Startup.cs
+++ MicroservicesApp/Ordering/Ordering.API/Startup.cs
@@ -8,9 +8,18 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Ordering.Application.Handlers;
+using Ordering.Core.Repositories;
+using Ordering.Core.Repositories.Base;
 using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories;
+using Ordering.Infrastructure.Repositories.Base;
 
 namespace Ordering.API
 {
@@ -30,6 +39,25 @@ namespace Ordering.API
 
             services.AddDbContext<OrderContext>(c =>
                 c.UseSqlServer(Configuration.GetConnectionString("OrderConnection")), ServiceLifetime.Singleton);
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddMediatR(typeof(CheckoutOrderHandler).GetTypeInfo().Assembly);
+
+            services.AddTransient<IOrderRepository, OrderRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Order API",
+                    Version = "V1"
+                });
+            });
+
+            // These AddScoped is here because of MediatR. Otherwise MediatR can't know the repositories.
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped(typeof(IOrderRepository), typeof(OrderRepository));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +75,13 @@ namespace Ordering.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API V1");
             });
         }
     }
